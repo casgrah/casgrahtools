a91f06eb68a6e202b2f9f3211ba3dd5fa50fff01
casgrah <caitlinsrock@gmail.com>
2026-06-03 22:21:32 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,15 @@
-const CACHE='cgt-v2';
-const ASSETS=['/casgrahtools/','/casgrahtools/index.html','/casgrahtools/bills.html',
-  '/casgrahtools/cxc.html','/casgrahtools/books.html','/casgrahtools/plants.html',
-  '/casgrahtools/manifest.json','/casgrahtools/icon.svg'];
+const CACHE='cgt-v3';
+const ASSETS=[
+  '/casgrahtools/',
+  '/casgrahtools/index.html',
+  '/casgrahtools/bills.html',
+  '/casgrahtools/debt.html',
+  '/casgrahtools/cxc.html',
+  '/casgrahtools/books.html',
+  '/casgrahtools/plants.html',
+  '/casgrahtools/manifest.json',
+  '/casgrahtools/icon.svg'
+];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(ca=>ca.put(e.request,c));return r;}).catch(()=>caches.match(e.request)));});
